a61be48efba622dce2af27a35b7552d418256052
gminova <minova1995@gmail.com>
2019-08-28 12:13:15 +0100

    move server to index.js file

--- src/index.js
+++ src/index.js
@@ -1,14 +1,5 @@
-const express = require("express"); //Handle routes & serve templates
-const path = require("path"); //Access and interact with the file system
+const app = require('./app');
 
-const app = express();
-
-//set up port
-app.set("port", process.env.PORT || 3000);
-
-//set up server
-const server = app.listen(app.get("port"), function () {
-    console.log("Express server listening on port " + server.address().port);
+app.listen(app.get('port'), () => {
+    console.log('App running on port: ', app.get('port'));
 });
-
-module.exports = server;
