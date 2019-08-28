677949c9aed081d65f4cfcd9c3f9addc1cdfa611
gminova <minova1995@gmail.com>
2019-08-28 15:20:33 +0100

    typos

--- src/tests/router.tests.js
+++ src/tests/router.tests.js
@@ -2,7 +2,8 @@ const test = require('tape');
 const supertest = require('supertest');
 const app = require('../app');
 
-test('Check we are ready for testing', t => {
+test('Check that we\'re ready for testing', t => {
     t.assert(true, true, 'Must return true');
     t.end();
-})
+});
+
