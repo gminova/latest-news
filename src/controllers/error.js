c12e835f199f0c650f52630482bca21347eba88f
gminova <minova1995@gmail.com>
2019-08-28 14:07:01 +0100

    fix error as per express syntax

--- src/controllers/error.js
+++ src/controllers/error.js
@@ -6,7 +6,7 @@ exports.client = (req, res) => {
     });
 };
 
-exports.server = (req, res) => {
+exports.server = (err, req, res, next) => {
     res.status(500).render('error', {
         layout: 'error',
         statusCode: 500,
