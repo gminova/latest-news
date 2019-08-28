8e6ae77186c01577b4e03fc507deb20ce377cf27
gminova <minova1995@gmail.com>
2019-08-28 14:01:43 +0100

    typo

--- src/controllers/error.js
+++ src/controllers/error.js
@@ -6,7 +6,7 @@ exports.client = (req, res) => {
     });
 };
 
-exports.server = (res, res) => {
+exports.server = (req, res) => {
     res.status(500).render('error', {
         layout: 'error',
         statusCode: 500,
