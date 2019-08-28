93b998601a2ac6aa61ea81bdaf563e7b78c2e07e
gminova <minova1995@gmail.com>
2019-08-28 15:25:28 +0100

    test home route

--- src/tests/router.tests.js
+++ src/tests/router.tests.js
@@ -7,3 +7,13 @@ test('Check that we\'re ready for testing', t => {
     t.end();
 });
 
+test('Check home route renders correctly', t => {
+    supertest(app)
+    .get('/')
+    .expect(200)
+    .expect('Content-type', /html/)
+    .end((err, res) => {
+        t.equals(null, err, 'No error with home route');
+        t.end();
+    });
+});
