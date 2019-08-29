a1d0dc66c6ed4bb36b69ba0fe59d32b6aa5efa28
gminova <minova1995@gmail.com>
2019-08-29 14:51:51 +0100

    fix typo with filepath

--- src/tests/db.tests.js
+++ src/tests/db.tests.js
@@ -1,7 +1,7 @@
 const test = require('tape');
-const runDbBuild = require("../src/model/database/db_build");
+const runDbBuild = require("../model/db_build");
 
-test('Check that we\'re ready for testing', t => {
+test('Check that we\'re ready for database testing', t => {
     t.assert(true, true, 'Must return true');
     t.assert(process.env.NODE_ENV, 'test', 'Must be in testing environment');
     t.end();
--- src/tests/router.tests.js
+++ src/tests/router.tests.js
@@ -2,7 +2,7 @@ const test = require('tape');
 const supertest = require('supertest');
 const app = require('../app');
 
-test('Check that we\'re ready for testing', t => {
+test('Check that we\'re ready for router testing', t => {
     t.assert(true, true, 'Must return true');
     t.assert(process.env.NODE_ENV, 'test', 'Must be in testing environment');
     t.end();
