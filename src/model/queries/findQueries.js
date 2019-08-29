d1e777a86f3bd2a28fbeebff478f386542d03619
gminova <minova1995@gmail.com>
2019-08-29 15:00:59 +0100

    basic logic for get queries, unfinished

---
+++ src/model/queries/findQueries.js
@@ -0,0 +1,9 @@
+const checkUsername = (username) => {
+    return false;
+}
+
+const hashedPassword = (hashedPassword) => {
+    return false;
+}
+
+module.exports = { checkUsername, hashedPassword }
--- src/tests/db.tests.js
+++ src/tests/db.tests.js
@@ -1,8 +1,20 @@
 const test = require('tape');
-const runDbBuild = require("../model/db_build");
+const runDbBuild = require('../model/db_build');
+const { checkUsername } = require('../model/queries/findQueries');
 
 test('Check that we\'re ready for database testing', t => {
     t.assert(true, true, 'Must return true');
     t.assert(process.env.NODE_ENV, 'test', 'Must be in testing environment');
     t.end();
+});
+
+test('Chesk is username already exists', t => {
+    runDbBuild((err, res) => {
+        t.error(err, null, "No error");
+        checkUsername()
+            .then(res => {
+                t.equals(Arrays.isArray(res.rows), true);
+                t.end();
+            });
+    });
 });
