ec4b9b39b213e4ca3b5a6085b4ecfddf86112a64
gminova <minova1995@gmail.com>
2019-08-30 23:14:24 +0100

    reg endpoint

--- src/controllers/index.js
+++ src/controllers/index.js
@@ -2,19 +2,27 @@ const express = require('express');
 const router = express.Router();
 
 //import authentication helpers
+const { createHash, compareHashes, createCookie, verifyCookie } = require('./helpers/authHelpers');
 
 //import database helpers
+const { createUser } = require('../model/queries/createQueries');
+const { deleteUser } = require('../model/queries/deleteQueries');
+const { findUsername, findHashedPassword } = require('../model/queries/readQueries');
+const { updateUsername, updatePassword } = require('../model/queries/updateQueries');
 
 //import home route controller
 const home = require('./home');
+const register = require('./register');
 const news = require('./news');
 const error = require('./error');
 
 //get home route
 router.get('/', home.get);
 //register route
-router.get('/register', req, res, next) => {
+router.post('/register', req, res, next) => {
     const { username, password } = req.body;
+    createHash(password)
+    createUser(username, )
 }
 router.get('/news', news.get);
 
