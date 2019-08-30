91cfe66ccd7d424bd311e854b04899c086529266
gminova <minova1995@gmail.com>
2019-08-30 12:52:02 +0100

    async function for checking user credentials

--- src/controllers/helpers/authHelpers.js
+++ src/controllers/helpers/authHelpers.js
@@ -1,5 +1,6 @@
 const bcrypt = require('bcrypt');
 const saltRounds = 10;
+const { findUsername, findHashedPassword } = require('../../model/queries/readQueries');
 
 //generate hashed password
 const hash = (password, cb) => {
@@ -15,6 +16,21 @@ const compareHashes = (password, hash, cb) => {
     bcrypt.compare(password, hash, (err, res) => {
         if (err) cb(err);
         cb(res);
+    });
+}
+
+//check user
+async function checkUser(username, password) {
+    findHashedPassword(username, (res) => {
+        if (res.rows.length === 0) {
+            throw new Error('Unable to find this user');
+        } else {
+            const match = await bcrypt.compare(password, res.rows[0].password_hash);
+            if (match) {
+                console.log('Successful log in');
+            }
+            console.log('Log in failed, please try again');
+        }
     });
 }
 
