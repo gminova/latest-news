1a75b9ee8a2c2bf9c89962ffb77c3bb7db32c3d6
gminova <minova1995@gmail.com>
2019-08-30 13:17:53 +0100

    test for comparing hashes

--- src/tests/auth.tests.js
+++ src/tests/auth.tests.js
@@ -7,12 +7,20 @@ test('Check that we\'re ready for authentication testing', t => {
     t.end();
 });
 
+test('Check password and hash match', t => {
+    const password = 'superSecret123^';
+    const hash = '$2b$10$JbtV/0BSh49Y34z74H9K4.gyeK3ByfCACDUhuToulKRMYUYbrHrjq';
+    compareHashes(password, hash, (res) => {
+        t.equals(res, true, 'Password is genuine');
+        t.end();
+    });
+});
+
 test('Check a password is hashed correctly', t => {
     const password = 'superSecret123^';
     hash(password, (hash) => {
-        console.log(hash)
         compareHashes(password, hash, (res) => {
-            t.deepEquals(res, true, 'Password is hashed correctly');
+            t.equals(res, true, 'Password is hashed correctly');
             t.end();
         });
     });
