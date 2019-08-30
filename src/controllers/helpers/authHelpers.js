9e138a4c092507586fd4cb52c0164f7e30f542d7
gminova <minova1995@gmail.com>
2019-08-30 11:55:21 +0100

    hashing function and test

---
+++ src/controllers/helpers/authHelpers.js
@@ -0,0 +1,21 @@
+const bcrypt = require('bcrypt');
+const saltRounds = 10;
+
+//generate hashed password
+const hash = (password, cb) => {
+    bcrypt.hash(password, saltRounds, (err, hash) => {
+        if (err) cb(err);
+        cb(hash);
+    });
+}
+
+
+//compare hashes
+const compareHashes = (password, hash, cb) => {
+    bcrypt.compare(password, hash, (err, res) => {
+        if (err) cb(err);
+        cb(res);
+    });
+}
+
+module.exports = { hash, compareHashes }
---
+++ src/tests/auth.tests.js
@@ -0,0 +1,18 @@
+const test = require('tape');
+const { hash, compareHashes } = require('../controllers/helpers/authHelpers');
+
+test('Check that we\'re ready for authentication testing', t => {
+    t.assert(true, true, 'Must return true');
+    t.assert(process.env.NODE_ENV, 'test', 'Must be in testing environment');
+    t.end();
+});
+
+test('Check a password is hashed correctly', t => {
+    const password = 'superSecret123^';
+    hash(password, (hash) => {
+        compareHashes(password, hash, (res) => {
+            t.deepEquals(res, true, 'Password is hashed correctly');
+            t.end();
+        });
+    });
+});
