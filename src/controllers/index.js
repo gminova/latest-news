c8bee0712dc1e7c50ed7085e96b8fd0169a66ddc
gminova <minova1995@gmail.com>
2019-08-31 03:02:08 +0100

    handle login

--- src/controllers/index.js
+++ src/controllers/index.js
@@ -16,7 +16,58 @@ const error = require('./error');
 
 //get home route
 router.get('/', (req, res) => {
-    res.render('home', { activePage: { home: true } });
+    if (!req.headers.cookie) {
+        res.render('home', { activePage: { home: true } });
+    } else {
+        const {
+            latestNews
+        } = parse(req.headers.cookie);
+        if (!latestNews) {
+            res.render('home', { activePage: { home: true } });
+        } else {
+            verifyCookie(latestNews, SECRET, (err, username) => {
+                if (err) {
+                    res.render('home', { activePage: { home: true } });
+                } else {
+                    const message = `You are logged in as: ${username}`;
+                    res.render('news', { loggedIn });
+                }
+            });
+        }
+
+    }
+});
+//login route
+router.get('/login', (req, res) => {
+    res.render('login');
+});
+//handle login route
+router.post('/login', (req, res, next) => {
+    let { username, password } = req.body; findHashedPassword(username, (dbRes) => {
+        //if username doesn't exists in database, send back a message
+        if (dbRes.length === 0) {
+            const message = "Sorry, you need to register first."
+            res.render("register", { message, main: true });
+        } else {
+            compareHashes(password, dbRes[0].password_hash, (err, match) => {
+                if (err) {
+                    const message = "Sorry, something went wrong. Try again."
+                    res.render("login", { message, main: true });
+                } else {
+                    if (!match) {
+                        const message = "Incorrect password. Try again."
+                        res.render("login", { message, main: true });
+                    } else {
+                        //generate cookie
+                        const cookie = createCookie(username);
+                        const week = 1000 * 60 * 60 * 24 * 7;
+                        res.cookie("latestNews", cookie, { maxAge: week * 1, httpOnly: true });
+                        res.render("news");
+                    }
+                }
+            });
+        }
+    });
 });
 //register route
 router.get("/register", (req, res) => {
@@ -37,7 +88,7 @@ router.post('/register', (req, res, next) => {
                     //generate cookie
                     const cookie = createCookie(username);
                     const week = 1000 * 60 * 60 * 24 * 7;
-                    res.cookie("latest-news", cookie, { maxAge: week * 1, httpOnly: true });
+                    res.cookie("latestNews", cookie, { maxAge: week * 1, httpOnly: true });
                     res.render("news");
                 });
             });
