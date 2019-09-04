72e4bb5adf47f0fbcb35d57b6bf01b453b69fd55
gminova <minova1995@gmail.com>
2019-09-04 01:23:44 +0100

    filter nyt stories into an array of stories

--- public/js/main.js
+++ public/js/main.js
@@ -1,6 +1,44 @@
 let url = '/fetchNews?=world';
 
-const filterResponse = (json) => console.log(json);
+//all articles ready to append to the DOM will be pushed here
+let stories = [];
+//each article will be an instance of the article constructor
+function article() {
+    this.title = undefined,
+        this.snippet = undefined,
+        this.img = undefined,
+        this.url = undefined,
+        this.author = undefined,
+        this.date = undefined,
+        this.paragraph = undefined,
+        this.source = undefined
+}
+
+//isolate filtering of original response
+const filterResponse = (json) => {
+    if (JSON.parse(json[0]).statusCode === 200) {
+        const news = JSON.parse(json[0]).body.response.docs;
+        console.log(news);
+
+        news.map(a => {
+            let story = new article();
+            story.title = a.headline.main;
+            story.snippet = a.snippet;
+            story.img = "https://www.nytimes.com/" + a.multimedia[0].url;
+            story.url = a.web_url;
+            story.author = a.byline.original;
+            story.date = a.pub_date.split("T")[0];
+            story.paragraph = a.lead_paragraph;
+            story.source = a.source;
+            stories.push(story);
+        });
+    }
+
+    if (JSON.parse(json[1]).statusCode === 200) {
+        const news2 = JSON.parse(json[1]).body;
+    }
+
+}
 
 const fetchNews = () => {
     fetch(url)
